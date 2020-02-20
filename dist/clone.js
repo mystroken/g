a60594438d6373a9418384348579706c0ee4e970
Emmanuel KWENE <njume48@gmail.com>
2020-02-20 19:19:11 +0100

    v0.0.14

---
+++ dist/clone.js
@@ -0,0 +1,1 @@
+var clone=function(){"use strict";return function(r){var n={};for(var t in r)n[t]=r[t];return n}}();
---
+++ dist/extend.js
@@ -0,0 +1,1 @@
+var extend=function(){"use strict";return function(r,n){var t=function(r){var n={};for(var t in r)n[t]=r[t];return n}(r);for(var e in n)t[e]=n.hasOwnProperty(e)?n[e]:r[e];return t}}();
